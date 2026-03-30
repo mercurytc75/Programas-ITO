d6f82fcbaed058ee1fca0ec71adcd36a0b385f41
mercurytc75 <mercurytc74@gmail.com>
2026-03-29 21:44:12 -0600

    Elimina las clases y las interfaces ColaGenerica, ColaTDA, PilaGenerica y PilaTDA, así como la clase Main, para limpiar el código y eliminar funcionalidades no utilizadas.

--- JAVA/ColasAndPilas/ColaTDA.java
+++
@@ -1,7 +0,0 @@
-public interface ColaTDA <T> {
-    void encolar(T elemento);
-    T desencolar();
-    T frente();
-    boolean estaVacia();
-    int tamanio();
-}
--- JAVA/ColasAndPilas/PilaTDA.java
+++
@@ -1,7 +0,0 @@
-public interface PilaTDA <T> {
-    void aplilar(T elemento);
-    T desapilar();
-    T clima();
-    boolean estaVacia();
-    int tamanio();
-}
