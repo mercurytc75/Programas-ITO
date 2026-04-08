5837856a997ca2c00b96712cf0b26db8899a7f93
mercurytc75 <mercurytc74@gmail.com>
2026-04-08 09:28:03 -0600

    feat: Eliminar lógica de consola en la clase Main y simplificar la inicialización del juego

--- JAVA/Monopoly/Monopoly/Main.java
+++ JAVA/Monopoly/Monopoly/Main.java
@@ -1,7 +1,6 @@
 package Monopoly;
 
-import controller.GameController;
-import model.Jugador;
+
 import ui.MonopolyFrame;
 
 import javax.swing.SwingUtilities;
@@ -9,20 +8,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
-        if (args != null && args.length > 0 && "consola".equalsIgnoreCase(args[0])) {
-            GameController gameController = new GameController();
-
-            Jugador j1 = new Jugador(1, "Juan");
-            Jugador j2 = new Jugador(2, "María");
-            Jugador j3 = new Jugador(3, "Carlos");
-
-            gameController.agregarJugador(j1);
-            gameController.agregarJugador(j2);
-            gameController.agregarJugador(j3);
-
-            gameController.iniciarJuego();
-            return;
-        }
+        
 
         SwingUtilities.invokeLater(() -> {
             MonopolyFrame frame = new MonopolyFrame();
